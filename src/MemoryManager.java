a39194dd481ee2f66550a8a22be99fc2ef234515
cdcarminucci <33530613+cdcarminucci@users.noreply.github.com>
2018-09-04 17:49:30 -0400

    Update MemoryManager.java

--- src/MemoryManager.java
+++ src/MemoryManager.java
@@ -1,149 +1,57 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+public class MemoryManager {
 
-/**
- * 
- */
+ // Constructor. poolsize defines the size of the memory pool in bytes
 
-/**
- * @author Christian
- *
- */
-public class CommandProcessor {
-    
-    private Hash hash;
-    private MemoryManager manager;
-    
-    public CommandProcessor(int memorySize, int hashSize, File file)
+    public MemoryManager(int poolsize) {
+
+        
+
+    }
+
+    /**
+     * temp function for making a handle. NOT FINAL VERSION
+     * @param name name string
+     * @return the handle
+     */
+    public Handle getHandle(String name)
     {
-        hash = new Hash(hashSize);
-        manager = new MemoryManager(memorySize);
-        Scanner scan = null;
-        try {
-            scan = new Scanner(file);
-        }
-        catch (FileNotFoundException e) {
-            System.out.println("Comman File Not Found");
-            e.printStackTrace();
-        }
-        while (scan.hasNextLine())
-        {
-            processCommand(scan.nextLine());
-        }
-        
+        Handle tempHandle = new Handle(5, 5, name); 
+        return tempHandle;
     }
-    
-    private boolean processCommand(String commandString) {
-        if (commandString.contains("update"))
-        {
-            update(commandString);
-            return true;
-        }
-        if (commandString.contains("add"))
-        {
-            add(commandString);
-            return true;
-        }
-        if (commandString.contains("delete"))
-        {
-            delete(commandString);
-            return true;
-        }
-        if (commandString.contains("print"))
-        {
-            print(commandString);
-            return true;
-        }
-        return false;
-        
-    }
+    // Insert a record and return its position handle.
 
-    private void update(String updateCommand) {
-        String updateShort = updateCommand.replace("update", "");
-        //boolean check;
-        if (updateCommand.contains("add"))
-        {
-            updateShort = updateShort.replace("add", "");
-            updateShort = formatString(updateShort);
-            String[] inputs = updateShort.split("<SEP>");
-            //check = hash.updateAdd(inputs[0], inputs[1], inputs[2]);
-           
-            System.out.println("|"+inputs[0]+"| todo");
-        } else {
-            updateShort = updateShort.replace("delete", "");
-            updateShort = formatString(updateShort);
-            String[] inputs = updateShort.split("<SEP>");
-            //check = hash.updateDelete(inputs[0], inputs[1]);
-            System.out.println("|"+inputs[0]+"| todo");
-        }
-        
-        
-    }
+    // space contains the record to be inserted, of length size.
 
-    private void print(String printCommand) {
-        if (printCommand.contains("hashtable"))
-        {
-            hash.print();
-        }
-        else {
-            System.out.println("Formatting?");
-        }
-        
-        
-    }
+//    public Handle insert(byte[] space, int size);
 
-    private void add(String addCommand)
-    {
-        String name;
-        name = addCommand.replace("add", "");
-        name = formatString(name);
-        boolean check;
-        Handle handle = manager.getHandle(name);
-        check = hash.add(name, handle);
-        if (check)
-        {
-            System.out.println("|"+name+"| has been added to the Name database.");
-        }
-        else {
-            System.out.println("|"+name+"| duplicates a record already in the Name database.");
-        }
-    }
     
-    private void delete(String deleteCommand)
-    {
-        String name;
-        name = deleteCommand.replace("delete", "");
-        name = formatString(name);
-        boolean check;
-        check = hash.remove(name);
-        if (check)
-        {
-            System.out.println("|"+name+"| has been deleted from the Name database.");
-        }
-        else {
-            System.out.println("|"+name+"| not deleted because it does not exist in the Name database.");
-        }
-    }
+
+    // Return the length of the record associated with theHandle
+
+//    public int length();
+
     
-    private String formatString(String nameString)
-    {
-        StringBuilder newString = new StringBuilder();
-        char[] chars = nameString.toCharArray();
-        for (int i = 0; i < chars.length; i++)
-        {
-            if (!Character.isWhitespace(chars[i]))
-            {
-                while (i < chars.length && !Character.isWhitespace(chars[i]))
-                {
-                    newString.append(chars[i]);
-                    i++;
-                }
-                newString.append(" ");
-            }
-        }
-        newString.deleteCharAt(newString.length()-1);
-        return newString.toString();
-    }
+
+    // Free a block at the position specified by theHandle.
+
+    // Merge adjacent free blocks.
+
+//    public void remove(Handle theHandle);
+
+    
+
+    // Return the record with handle posHandle, up to size bytes, by
+
+    // copying it into space.
+
+    // Return the number of bytes actually copied into space.
+
+//    public int get(byte[] space, Handle theHandle, int size);
+
+    
+
+    // Dump a printout of the freeblock list
+
+//    public void dump();
 
 }
