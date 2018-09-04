b780858a044d38e5d610afb0fc02c8b89baaaa85
cdcarminucci <33530613+cdcarminucci@users.noreply.github.com>
2018-09-04 19:35:30 -0400

    Update CommandProcessor.java

--- src/CommandProcessor.java
+++ src/CommandProcessor.java
@@ -3,20 +3,35 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 /**
+ * 
  * @author cdc97 bob313
- * Processes command file and feeds instructions Hash and MemoryManager.
+ * 
+ *         Processes command file and feeds instructions Hash and MemoryManager.
+ * 
  */
+
 public class CommandProcessor {
-    
     private Hash hash;
     private MemoryManager manager;
-    
+
+
     /**
-     * Constructor for CommandProcessor. Takes inputs from main method and processes the commands.
-     * @param memorySize size of MemoryManager received from main method
-     * @param hashSize size of Hash received from main method
-     * @param file the input file containing the commands to be read and processed
+     * 
+     * Constructor for CommandProcessor. Takes inputs from main method and
+     * processes the commands.
+     * 
+     * @param memorySize
+     *            size of MemoryManager received from main method
+     * 
+     * @param hashSize
+     *            size of Hash received from main method
+     * 
+     * @param file
+     *            the input file containing the commands to be read and
+     *            processed
+     * 
      */
+
     public CommandProcessor(String memorySize, String hashSize, String file)
     {
         hash = new Hash(Integer.valueOf(hashSize));
@@ -33,13 +48,19 @@ public class CommandProcessor {
         {
             processCommand(scan.nextLine());
         }
-        
     }
-    
+
+
     /**
-     * Processes a given line of the command file for the command found in the line
-     * @param commandString the line of the command file being processed
+     * 
+     * Processes a given line of the command file for the command found in the
+     * line
+     * 
+     * @param commandString
+     *            the line of the command file being processed
+     * 
      * @return true if proper command present false if not
+     * 
      */
     private boolean processCommand(String commandString) {
         if (commandString.contains("update"))
@@ -63,65 +84,69 @@ public class CommandProcessor {
             return true;
         }
         return false;
-        
     }
 
+
     /**
-     * Handles the 
+     * 
+     * Handles the
+     * 
      * @param updateCommand
+     * 
      */
     private void update(String updateCommand) {
         String updateShort = updateCommand.replace("update", "");
-        //boolean check;
+        // boolean check;
         if (updateCommand.contains("add"))
         {
             updateShort = updateShort.replace("add", "");
             updateShort = formatString(updateShort);
             String[] inputs = updateShort.split("<SEP>");
-            //check = hash.updateAdd(inputs[0], inputs[1], inputs[2]);
-           
-            System.out.println("|"+inputs[0]+"| todo");
-        } else {
+            // check = hash.updateAdd(inputs[0], inputs[1], inputs[2]);
+            System.out.println("|" + inputs[0] + "| todo");
+        }
+        else {
             updateShort = updateShort.replace("delete", "");
             updateShort = formatString(updateShort);
             String[] inputs = updateShort.split("<SEP>");
-            //check = hash.updateDelete(inputs[0], inputs[1]);
-            System.out.println("|"+inputs[0]+"| todo");
+            // check = hash.updateDelete(inputs[0], inputs[1]);
+            System.out.println("|" + inputs[0] + "| todo");
         }
-        
-        
     }
 
+
     private void print(String printCommand) {
-        if (printCommand.contains("hashtable"))
+        if (printCommand.contains("blocks"))
         {
-            hash.print();
+            System.out.println(manager.getPoolSize()+": 0");
         }
         else {
-            System.out.println("Formatting?");
+            hash.print();
+            
         }
-        
-        
     }
+
 
     private void add(String addCommand)
     {
         String name;
         name = addCommand.replace("add", "");
         name = formatString(name);
-        System.out.println("NAME: " + name);
         boolean check;
         Handle handle = manager.getHandle(name);
         check = hash.add(name, handle);
         if (check)
         {
-            System.out.println("|"+name+"| has been added to the Name database.");
+            System.out.println("|" + name
+                + "| has been added to the Name database.");
         }
         else {
-            System.out.println("|"+name+"| duplicates a record already in the Name database.");
+            System.out.println("|" + name
+                + "| duplicates a record already in the Name database.");
         }
     }
-    
+
+
     private void delete(String deleteCommand)
     {
         String name;
@@ -131,13 +156,16 @@ public class CommandProcessor {
         check = hash.remove(name);
         if (check)
         {
-            System.out.println("|"+name+"| has been deleted from the Name database.");
+            System.out.println("|" + name
+                + "| has been deleted from the Name database.");
         }
         else {
-            System.out.println("|"+name+"| not deleted because it does not exist in the Name database.");
+            System.out.println("|" + name
+                + "| not deleted because it does not exist in the Name database.");
         }
     }
-    
+
+
     private String formatString(String nameString)
     {
         StringBuilder newString = new StringBuilder();
@@ -154,8 +182,7 @@ public class CommandProcessor {
                 newString.append(" ");
             }
         }
-        newString.deleteCharAt(newString.length()-1);
+        newString.deleteCharAt(newString.length() - 1);
         return newString.toString();
     }
-
 }
