7a6ca27c976520ba97923e490cb7bb99cbf8cb98
cdcarminucci <33530613+cdcarminucci@users.noreply.github.com>
2018-09-04 18:34:23 -0400

    Update CommandProcessorTest.java

--- src/CommandProcessorTest.java
+++ src/CommandProcessorTest.java
@@ -1,14 +1,11 @@
 import student.TestCase;
-import java.io.File;
 
 public class CommandProcessorTest extends TestCase{
     CommandProcessor processor;
-    File testFile;
     
     public void testConstructor()
     {
-        testFile = new File("C:\\Users\\Christian\\Downloads\\P1sampleInput.txt");
-        processor = new CommandProcessor(32, 32, testFile);
+        processor = new CommandProcessor("32", "32", "P1sampleInput.txt");
         int x = 8;
         assertEquals(x, 8);
     }
