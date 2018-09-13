afa4805fcf30f3526f3af71b092551901badf24d
cdcarminucci <33530613+cdcarminucci@users.noreply.github.com>
2018-09-13 15:30:22 -0400

    Update MemoryManagerTest.java

--- src/MemoryManagerTest.java
+++ src/MemoryManagerTest.java
@@ -8,6 +8,25 @@ public class MemoryManagerTest extends TestCase {
         manager = new MemoryManager(32);
         assertEquals(1, manager.getBlockList().get(0).getList().size());
         manager.dump();
+        
+        String input1 = "Christian Carminucci";
+        Handle testHandle = manager.insert(input1.getBytes(), input1.getBytes().length, input1);
+        assertTrue(manager.getBlockList().isEmpty());
+        manager.dump();
+        
+        manager.remove(testHandle);
+        assertEquals(1, manager.getBlockList().size());
+        manager.dump();
+        
+        String input2 = "Christian";
+        Handle testHandle2 = manager.insert(input2.getBytes(), input2.getBytes().length, input2);
+        assertEquals(1, manager.getBlockList().size());
+        manager.dump();
+        
+        String input3 = "A really long string";
+        Handle testHandle3 = manager.insert(input3.getBytes(), input3.getBytes().length, input3);
+        assertEquals(2, manager.getBlockList().size());
+        manager.dump();
     }
 
 
@@ -18,10 +37,39 @@ public class MemoryManagerTest extends TestCase {
         assertEquals(0, manager.insert(input.getBytes(), input
             .getBytes().length, input).getMemPool());
         assertEquals(1, manager.getBlockList().size());
-        //manager2 = new MemoryManager(32);
         String input2 = "p";
         assertEquals(1, manager.insert(input2.getBytes(), input2
             .getBytes().length, input2).getLength());
+        String input3 = "A Really Long Word";
+        assertEquals(18, manager.insert(input3.getBytes(), input3
+            .getBytes().length, input3).getLength());
+        manager2 = new MemoryManager(0);
+        String input4 = "Christian";
+        assertEquals(0, manager2.insert(input4.getBytes(), input4
+            .getBytes().length, input4).getMemPool());
+    }
+    
+    public void testRemove() {
+        manager = new MemoryManager(32);
+        assertEquals(1, manager.getBlockList().size());
+        String input = "Christian";
+        Handle testHandle = manager.insert(input.getBytes(), input
+            .getBytes().length, input);
+        manager.remove(testHandle);
+        assertEquals(32, manager.getBlockList().get(0).getSize());
+        
+        manager2 = new MemoryManager(32);
+        String input2 = "Christian";
+        Handle testHandle2 = manager2.insert(input2.getBytes(), input2
+            .getBytes().length, input2);
+        String input3 = "William Bao";
+        Handle testHandle3 = manager2.insert(input3.getBytes(), input3
+            .getBytes().length, input3);
+        manager2.remove(testHandle2);
+        assertEquals(16, manager2.getBlockList().get(0).getSize());
+        manager2.remove(testHandle3);
+        assertEquals(32, manager2.getBlockList().get(0).getSize());
     }
 
+    
 }
